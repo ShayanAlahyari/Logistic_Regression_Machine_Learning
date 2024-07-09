899cf184c1ef3d92d3d0d49deaf8fcb0b3b9fbed
shayanalahyari <shayanforstock@gmail.com>
2024-07-09 14:54:34 +0330

    ...

--- logistic_regression/logistic_regression.py
+++ logistic_regression/logistic_regression.py
@@ -2,8 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Creating the dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
 x = dataset.iloc[:,:-1].values
-y = np.array(dataset.iloc[:,-1].values).reshape(40,10)
-
-print(y)
+y = dataset.iloc[:,-1].values
