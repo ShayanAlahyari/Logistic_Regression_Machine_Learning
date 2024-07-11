497652b9b31ab05813793f3575165b5070838b6d
Shayan Alahyari <shayanforstock@gmail.com>
2024-07-11 18:46:02 +0330

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,4 +1,3 @@
-# Importing libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -11,9 +10,9 @@ dataset = pd.read_csv('Social_Network_Ads.csv')
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-# There are no missing values or categorical features in the dataset
+# There no missing value or categorical feature in the dataset
 
-# Splitting the data into training and test sets
+# Splitting the data into training and test set
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
 
 # Feature scaling
@@ -21,7 +20,11 @@ scalar = StandardScaler()
 x_train = scalar.fit_transform(x_train)
 x_test = scalar.transform(x_test)
 
-# Training the model on our training set
+# Training the model on our training and test sets
 classifier = LogisticRegression()
-y_pre_train = classifier.fit(x_train, y_train)
+classifier.fit(x_train, y_train)
 
+print(classifier.predict(scalar.transform([[30, 87000]])))
+
+y_pred = classifier.predict(x_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
