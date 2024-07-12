2031f6c6031d773414d3dbdc05abd3266c567d54
Shayan Alahyari <shayanforstock@gmail.com>
2024-07-12 08:37:19 +0330

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 
 # Creating the dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
@@ -13,7 +15,7 @@ y = dataset.iloc[:, -1].values
 # There no missing value or categorical feature in the dataset
 
 # Splitting the data into training and test set
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=0)
 
 # Feature scaling
 scalar = StandardScaler()
@@ -24,7 +26,49 @@ x_test = scalar.transform(x_test)
 classifier = LogisticRegression()
 classifier.fit(x_train, y_train)
 
-print(classifier.predict(scalar.transform([[30, 87000]])))
+# print(classifier.predict(scalar.transform([[30, 87000]])))
 
 y_pred = classifier.predict(x_test)
-print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+# print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+
+confusion_matrix = confusion_matrix(y_test, y_pred)
+accuracy_score = accuracy_score(y_test, y_pred)
+# print(confusion_matrix)
+# print(accuracy_score)
+
+
+from matplotlib.colors import ListedColormap
+
+X_set, y_set = scalar.inverse_transform(x_train), y_train
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 10, stop=X_set[:, 0].max() + 10, step=0.25),
+                     np.arange(start=X_set[:, 1].min() - 1000, stop=X_set[:, 1].max() + 1000, step=0.25))
+plt.contourf(X1, X2, classifier.predict(scalar.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('salmon', 'dodgerblue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=ListedColormap(('salmon', 'dodgerblue'))(i), label=j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = scalar.inverse_transform(x_test), y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, classifier.predict(scalar.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('salmon', 'dodgerblue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('salmon', 'dodgerblue'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
